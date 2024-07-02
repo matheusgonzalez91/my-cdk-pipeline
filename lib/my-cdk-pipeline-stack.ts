c741ee28f6a592e2354f9cac2c310cde38d679b4
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-07-02 13:37:10 -0300

    pipeline

--- lib/my-cdk-pipeline-stack.ts
+++ lib/my-cdk-pipeline-stack.ts
@@ -1,44 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as glue from 'aws-cdk-lib/aws-glue';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class MyCdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // S3 bucket to store pipeline artifacts
-    const artifactBucket = new s3.Bucket(this, 'ArtifactBucket');
+    // Criação do bucket S3 para artefatos do pipeline
+    const artifactBucket = new s3.Bucket(this, 'PipelineArtifactBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Remover o bucket quando o stack for destruído
+    });
 
-    // IAM role for Glue job
-    const jobRole = new iam.Role(this, 'JobRole', {
-      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'),
+    // Bucket S3 para armazenar scripts de ETL do Glue
+    const glueScriptsBucket = new s3.Bucket(this, 'GlueScriptsBucket', {
+      removalPolicy: cdk.RemovalPolicy.RETAIN, // Manter o bucket quando o stack for destruído
+    });
+
+    // Role do IAM para o CodePipeline acessar o Secrets Manager
+    const pipelineRole = new iam.Role(this, 'PipelineRole', {
+      assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
+    });
+
+    pipelineRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        'secretsmanager:GetSecretValue',
       ],
-    });
+      resources: [
+        `arn:aws:secretsmanager:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:secret:AWS_GITHUB_TOKEN-*`,
+      ],
+    }));
 
-    // Glue job definition
-    const glueJob = new glue.CfnJob(this, 'MyGlueJob', {
-      role: jobRole.roleArn,
-      command: {
-        name: 'glueetl',
-        scriptLocation: 's3://myprontuariobucket/results/job_prontuario.py',
-      },
-      defaultArguments: {
-        '--job-language': 'python',
-        '--additional-python-modules': 'pyspark==2.4.3',
-      },
-    });
-
-    // Output the bucket name and Glue job name
-    new cdk.CfnOutput(this, 'ArtifactBucketName', {
-      value: artifactBucket.bucketName,
-    });
-
-    new cdk.CfnOutput(this, 'GlueJobName', {
-      value: glueJob.ref,
-    });
+    // Definir o segredo do GitHub Token
+    const gitHubToken = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubToken', 'AWS_GITHUB_TOKEN');
+    
   }
 }
