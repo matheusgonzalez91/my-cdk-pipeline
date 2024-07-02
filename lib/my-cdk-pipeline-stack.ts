5d5da91ee6d0966840acd212151c95e2436fb696
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-07-02 10:44:56 -0300

    pipeline

--- lib/my-cdk-pipeline-stack.ts
+++ lib/my-cdk-pipeline-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as glue from 'aws-cdk-lib/aws-glue';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class MyCdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3 bucket to store pipeline artifacts
+    const artifactBucket = new s3.Bucket(this, 'ArtifactBucket');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM role for Glue job
+    const jobRole = new iam.Role(this, 'JobRole', {
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'),
+      ],
+    });
+
+    // Glue job definition
+    const glueJob = new glue.CfnJob(this, 'MyGlueJob', {
+      role: jobRole.roleArn,
+      command: {
+        name: 'glueetl',
+        scriptLocation: 's3://myprontuariobucket/results/job_prontuario.py',
+      },
+      defaultArguments: {
+        '--job-language': 'python',
+        '--additional-python-modules': 'pyspark==2.4.3',
+      },
+    });
+
+    // Output the bucket name and Glue job name
+    new cdk.CfnOutput(this, 'ArtifactBucketName', {
+      value: artifactBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'GlueJobName', {
+      value: glueJob.ref,
+    });
   }
 }
